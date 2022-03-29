7e61eee272fba33351b3eb219f7f2b7066f0fccc
shun yamasaka <118syuunico@gmail.com>
2022-03-30 00:06:00 +0900

    各ユーザコントロール作成

--- 03_開発/TASKBoard/TASKBoard/TODO_Control.Designer.cs
+++ 03_開発/TASKBoard/TASKBoard/TODO_Control.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace TASKBoard
 {
-    partial class Form1
+    partial class TODO_Control
     {
-        /// <summary>
+        /// <summary> 
         /// 必要なデザイナー変数です。
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// 使用中のリソースをすべてクリーンアップします。
         /// </summary>
         /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
@@ -20,40 +20,26 @@
             base.Dispose(disposing);
         }
 
-        #region Windows フォーム デザイナーで生成されたコード
+        #region コンポーネント デザイナーで生成されたコード
 
-        /// <summary>
-        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
         /// コード エディターで変更しないでください。
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // panel1
-            // 
-            this.panel1.Location = new System.Drawing.Point(-3, -3);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1051, 796);
-            this.panel1.TabIndex = 0;
-            // 
-            // Form1
+            // TODO_Control
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 792);
-            this.Controls.Add(this.panel1);
-            this.MaximizeBox = false;
-            this.Name = "Form1";
-            this.Text = "TaskBoard";
+            this.Name = "TODO_Control";
+            this.Size = new System.Drawing.Size(1051, 796);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel1;
     }
 }
-
